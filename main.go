8902252bb1eae490c821de772d66b6e7c001e776
db_hou <houdaibing@gmail.com>
2018-04-10 00:42:56 +0800

    init project

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	_ "cms/routers"
+	"mime"
+	"os"
+
+	. "cms/lib"
+	"cms/models"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	//初始化
+	initialize()
+
+	beego.Run()
+}
+
+func initialize() {
+	mime.AddExtensionType(".css", "text/css")
+	//判断初始化参数
+	initArgs()
+
+	models.Connect()
+
+	beego.AddFuncMap("stringsToJson", StringsToJson)
+}
+
+func initArgs() {
+	args := os.Args
+	for _, v := range args {
+		if v == "-syncdb" {
+			models.Syncdb()
+			os.Exit(0)
+		}
+	}
+}
